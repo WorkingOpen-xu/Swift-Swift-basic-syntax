b186933e2a169d135a1f034edf7ead15d49873bc
xujiahuicoin <xujiahuicoin@gmail.com>
2020-07-08 23:24:34 +0800

    no message

--- Swift/swift语法2.0-四/3协议代理.playground/Contents.swift
+++ Swift/swift语法2.0-四/3协议代理.playground/Contents.swift
@@ -1,151 +1,122 @@
 import UIKit
 
-//MARK --- 协议 基础
-protocol Pet {
-    var name: String{get set}
-    
-    func playWith()
-    func fed()
-    
-    init(name:String)
+protocol Record: CustomStringConvertible {
+    var win: Int {get}
+    var losses: Int {get}
+
 }
 
-class Animal {
-    var typr: String = "mammal"
-}
-
-class Dog: Animal, Pet {
-    required init(name: String) {
-        self.name = name
-    }
-    
-    
-    func playWith() {
-        
-    }
-    
-    func fed() {
-        
-    }
-    
-    var name: String = "Puppy"
-    
+protocol tieable {
+    var ties: Int { get }
 }
 
 
-class Bird: Animal {
-    var name: String = "yingwu"
-    init(name:String) {
-        self.name = name
-    }
+protocol jiangli {
+    func canFa() -> Bool
 }
 
-
-class Parrot: Bird, Pet {
-    func playWith() {
-        
+extension Record where Self: tieable {
+    
+    var geamPlayed: Int {
+        return win + losses + ties
+    }
+    func winningtaaa() -> Int {
+        return win * losses * ties
+    }
+}
+//默认实现
+extension Record {
+    
+    func shortWin() -> Int {
+        return win
     }
     
-    func fed() {
-        
+    var geamPlayed: Int {
+        return win + losses
     }
     
-    required override init(name: String) {
-        super.init(name: name )
+    var description: String {
+        return "我这次比赛胜利了：\(win) 场，输了 \(losses)"
     }
     
-    
-}
-
-
-// MARK -- 类型别名-- 关联类型。  typealias
-
-typealias Length = Double
-
-extension Length{
-    var km: Length { return self * 1000 }
-    var m: Length { return self }
-    var cm: Length {return self / 100 }
-    var ft: Length { return self / 3.2808}
-}
-
-let runingDistance: Length = 3.54.km
-
-typealias AudioSample = UInt32
-
-//--------协议中使用别名 要用 associatedtype
-
-//协议 于 别名使用的情况
-protocol WeightCalculable {
-    associatedtype weightType
-    var weight: weightType { get }
-}
-
-class iPhone7: WeightCalculable {
-    typealias weightType = Double
-    var weight: weightType {
-        return 0.0114
-    }
-  
-}
-
-class Ship: WeightCalculable {
-    typealias weightType = Int
-    var weight: weightType
-    init(weight: Int) {
-        self.weight = weight
+    func winningtaaa() -> Int {
+        return win * losses
     }
 }
 
-// 对int 做一个扩展 t代表吨，。t相当于*1000
-extension Int {
-    typealias weight = Int
-    var t: weight { return self * 1_000 }
-}
 
-let titanic = Ship(weight: 46_328.t)
-
-
-//MARK---标准库中的常用协议: 比较协议Equatable、 比较大小Comparable, 打印协议 CustomStringConvertible
-
-struct Record: Equatable, Comparable, CustomStringConvertible, BooleanLiteralType {
-  
-    var wins: Int
+struct basketballRecord: Record, jiangli {
+    func  canFa() -> Bool {
+        return win > losses
+    }
+    
+    var win: Int
+    
     var losses: Int
     
-    static func < (lhs: Record, rhs: Record) -> Bool {
-        if lhs.wins != rhs.wins {
-            return lhs.wins < rhs.wins
-        }
-        
-        return lhs.losses > rhs.losses
-    }
+}
 
-    var description: String {
-           return "x Win:" + "\(self.wins)" + "Loss:" + "\(self.losses)"
-       }
+
+struct football: Record, tieable, jiangli{
     
-    var boolValue: Bool {
-        return wins > losses
+    func canFa() ->  Bool {
+        return win > losses  && win > ties
+    }
+    
+    var win: Int
+    
+    var losses: Int
+    
+    var ties: Int
+}
+
+struct baseballRecord : Record {
+    var win: Int
+    var losses: Int
+    
+    var geamPlayed: Int = 1623
+}
+
+
+let xiaoming = basketballRecord(win: 12, losses: 12)
+print(xiaoming)
+
+xiaoming.shortWin()
+
+let zuqiu = football(win: 12, losses: 2, ties: 45)
+zuqiu.geamPlayed
+print(zuqiu)
+
+zuqiu.shortWin()
+
+
+
+if zuqiu.canFa() {
+    print(zuqiu)
+}
+
+func award(one: CustomStringConvertible & jiangli) {
+    if one.canFa() {
+        
+        print(one)
+        
+    } else {
+        print(one)
     }
 }
 
-let recordA = Record(wins: 10, losses: 10)
-let recordB = Record(wins: 13, losses: 10)
-let recordC = Record(wins: 11, losses: 9)
-
-var records = [recordA, recordB, recordC]
-recordA == recordB
-recordA != recordB
-
-
-recordB > recordA
-recordB < recordA
-
-records.sort(by: >)
-
-print(recordA)
-
-if recordC {
-    print("wins")
+struct studet: jiangli, CustomStringConvertible {
+    func canFa() -> Bool {
+        return fenshu > 60
+    }
+    
+    var description: String {
+        return name + "kaoshi\(fenshu)"
+    }
+    var fenshu: Int
+    var name: String
 }
+
+let xiaoxiao = studet(fenshu: 78, name: "xiaonig")
+
+award(one: xiaoxiao)
